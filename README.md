26c7605e239ef3f9dee8a17093ddbe7f419e43f5
Admin <admin@pludoni.de>
2012-02-19 14:44:42 +0100

    done

---
+++ README.md
@@ -0,0 +1,82 @@
+# Repost lib for Jekyll blog
+
+
+I needed a convenient way to repost stuff in a jekyll blog, which i posted in another blog (corporate blog etc).
+So i mungled together this gem, which uses Feedzirra as interface, so can handle both RSS and Atom Feeds.
+
+## Install
+
+add to Gemfile or install with gem install:
+
+```ruby
+gem "jekyll_reposter"
+```
+
+## Usage
+
+create a ruby file in the jekyll root folder or subfolder like "tools"/"import"
+etc. and fill in:
+
+```ruby tools/notes.rb
+require "jekyll_reposter"
+
+reposter = Jekyll::Reposter.new "http://notes.it-jobs-und-stellen.de/notes.atom",
+  :tags => "[notes, external]", :pretend => true
+
+reposter.create_if do |entry|
+  true
+end
+```
+
+This will create all blog posts, if not exists yet inside my
+```source/_posts/notes.it-jobs-und-stellen.de`` folder.
+
+The ```create_if``` directive decideds if a blog posts is created. So if you
+want to filter the passed feed, like to only show specific authors posts, then
+here you can add any logic. In our case, we post all new items.
+
+
+After finshed, you can run that script like
+```bash
+ruby tools/notes.rb
+```
+any time in your workflow, to add all new posts. Afterwards, check formatting and
+add categories if necessary.
+
+## Config
+
+There are some options, you can pass to the Reposter:
+
+```ruby
+:tags         => "[notes, external]",   #categories
+:dir          => "source/_posts",
+:allowed_tags => %w[h2 ul li ol h3 h4 h5 code pre quote blockquote cite hr],
+:meta => {   #default markdown meta tags
+  "comments" => true,
+  "layout" => "post"
+},
+:pretend => false   # do not create posts, but show the generated output
+```
+
+Additionally, there are some replacings to make a better output, you can
+customize that with the attribute "reposter.replacings", default are:
+
+```ruby
+reposter.replacings = {
+  "“"       => '"',
+  "”"       => '"',
+  "&lt;"    => "<",
+  "&gt;"    => ">",
+  "</li>"   => "",
+  "</ul>"   => "",
+  "<ul>"    => "",
+  "<pre>"   => "\n\n```ruby\n",
+  "</pre>"  => "```\n",
+  /\s+<li>/ => "* "
+}
+```
+
+Also, for SEO reasons, a link to the original post will be added at the end of
+each entry with a rel=canonical.
+
+
---
+++ lib/jekyll-reposter.rb
@@ -0,0 +1,99 @@
+# encoding: utf-8
+
+require "active_support"
+require 'feedzirra'
+require "fileutils"
+require "uri"
+require "stringex"
+require "sanitize"
+require "yaml"
+
+module Jekyll
+  class Reposter
+    VERSION = "0.0.1"
+
+    attr_accessor :replacings
+
+    # fetching a single feed
+    def initialize(feed, options={})
+      @options = {
+        :tags         => "[notes, external]",
+        :dir          => "source/_posts",
+        :allowed_tags => %w[h2 ul li ol h3 h4 h5 code pre quote blockquote cite hr],
+        :meta => {
+          "comments" => true,
+          "layout" => "post"
+        },
+        :pretend => false
+       }.merge options
+
+      @replacings = {
+          "“"       => '"',
+          "”"       => '"',
+          "&lt;"    => "<",
+          "&gt;"    => ">",
+          "</li>"   => "",
+          "</ul>"   => "",
+          "<ul>"    => "",
+          "<pre>"   => "\n\n```ruby\n",
+          "</pre>"  => "```\n",
+          /\s+<li>/ => "* "
+        }
+
+      @feed = Feedzirra::Feed.fetch_and_parse(feed)
+      @uri = URI.parse(feed)
+      @dir = File.join @options[:dir], @uri.host
+      FileUtils.mkdir_p(@dir)
+      puts "Working dir: #{@dir}"
+    end
+
+    # creates the blog entry file, if passed block
+    # returns true. block is a entry object from
+    # feedzirra, like filtering
+    def create_if(&block)
+      @feed.entries.each do |entry|
+        if block.call(entry)
+          create_entry(entry)
+        end
+      end
+    end
+
+    def create_entry(entry)
+      date = entry.published.strftime("%Y-%m-%d")
+      filename = File.join @options[:dir], date + "-" + entry.title.to_url + ".markdown"
+      unless File.exists? filename
+        content =  Sanitize.clean entry.content, :elements => @options[:allowed_tags]
+
+      end
+      @replacings.each do |from,to|
+        content.gsub! from, to
+      end
+
+      meta = {
+              "title" => entry.title,
+              "date" => entry.published.strftime("%Y-%m-%d %H:%M"),
+      }.merge(@options[:meta])
+
+      tags = @options[:tags]
+      file = <<DOC
+#{meta.to_yaml}
+categories: #{tags}
+---
+#{content}
+
+---
+<i>Repost from <a href='#{entry.url}' rel='canonical'>#{@uri.host}</a></i>
+DOC
+
+      if !@options[:pretend]
+        File.open filename, "w+" do |f|
+          puts "Writing #{filename}"
+          f.write file
+        end
+      else
+        puts "Would create #{filename} with content:\n------------------\n#{file}"
+      end
+
+    end
+  end
+end
